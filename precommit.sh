e54c483f320edcbabb3263b3f5ac07cebd76ee0c
nick3point5 <kenny.trinh4444@gmail.com>
2023-03-06 21:38:05 -0600

    patch

--- precommit.sh
+++ precommit.sh
@@ -3,9 +3,10 @@
 # Get the current version from package.json
 VERSION=$(node -pe "require('./package.json').version")
 
+echo hello
 # Increment the version number using the npm version command
-npm version $VERSION --no-git-tag-version
-npm lint:fix
+npm version patch
+npm run lint:fix
 
 # Add the updated package.json to the commit
 git add package.json
