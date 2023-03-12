e611ce834423e0f58308d76ee26358801a9bb4b8
nick3point5 <ktt4444@gmail.com>
2023-03-12 01:56:33 -0600

    parseString test

---
+++ src/utils/parseTokens/parseTokens.test.js
@@ -0,0 +1,20 @@
+import { expect } from "vitest"
+import { test, describe } from "vitest"
+import { parseTokens, Template } from "./parseTokens.js"
+
+const settings = {
+	"barrel_imports": false,
+	"relative": false,
+	"templatesSource": "C:/code/side-bits/em-gen/test/em-gen-templates",
+	"baseUrl": "C:/code/side-bits/em-gen/test"
+}
+
+describe('Template class', () => {
+	test('', () => {
+		
+	})
+})
+
+// describe('parse tokens into templates', () => {
+
+// })
