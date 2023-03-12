57bf9b6643ebe80ec6d813e9ff916c8ba8495444
nick3point5 <ktt4444@gmail.com>
2023-03-12 06:36:15 -0500

    setting for testing

--- src/utils/parseTokens/parseTokens.test.js
+++ src/utils/parseTokens/parseTokens.test.js
@@ -3,14 +3,10 @@ import { test, describe } from "vitest"
 import { parseTokens, Template } from "./parseTokens.js"
 import { parseString } from "../parseString/parseString.js"
 import { parseEmmet } from "../parseEmmet/parseEmmet.js"
+import { getConfig } from "../getConfig.js"
 
 
-const settings = {
-	"barrel_imports": false,
-	"relative": false,
-	"templatesSource": "C:/code/side-bits/em-gen/test/em-gen-templates",
-	"baseUrl": "C:/code/side-bits/em-gen/test"
-}
+const settings = getConfig()
 
 describe('should parse tokens', () => {
 	test('siblings', () => {
