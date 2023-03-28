da90f890ae2e5815558047d1b920ec4511148010
nick3point5 <ktt4444@gmail.com>
2023-03-27 22:31:47 -0500

    format JavaScript

--- dist/utils/generateInit/generateInit.js
+++ dist/utils/generateInit/generateInit.js
@@ -1,19 +1,19 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { InitTemplate } from '../InitTemplate/InitTemplate.js';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { InitTemplate } from '../InitTemplate/InitTemplate.js'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 export function generateInit(type) {
-    let sourcePath;
-    if (type === 'test') {
-        sourcePath = path.resolve(`${__dirname}/../../data/initTest.json`);
-    }
-    else {
-        sourcePath = path.resolve(`${__dirname}/../../data/init.json`);
-    }
-    const source = fs.readFileSync(sourcePath, 'utf8');
-    const destPath = path.resolve(process.cwd());
-    InitTemplate.createInit(destPath, source);
+	let sourcePath
+	if (type === 'test') {
+		sourcePath = path.resolve(`${__dirname}/../../data/initTest.json`)
+	}
+	else {
+		sourcePath = path.resolve(`${__dirname}/../../data/init.json`)
+	}
+	const source = fs.readFileSync(sourcePath, 'utf8')
+	const destPath = path.resolve(process.cwd())
+	InitTemplate.createInit(destPath, source)
 }
 //# sourceMappingURL=generateInit.js.map
--- dist/utils/getConfig.js
+++ dist/utils/getConfig.js
@@ -1,31 +1,31 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 export function getConfig(isAbsolute) {
-    let configLocation = `${process.cwd()}/emmet-gen-templates.json`;
-    let previous = null;
-    while (!fs.existsSync(configLocation)) {
-        previous = configLocation;
-        configLocation = path.resolve(`${configLocation}/../../emmet-gen-templates.json`);
-        if (previous === configLocation) {
-            console.error('no emmet-gen-templates.json found');
-            process.exit(1);
-        }
-    }
-    const config = JSON.parse(fs.readFileSync(configLocation).toString());
-    const templatesSrc = path.resolve(configLocation, '..', config.templatesSource);
-    if (!fs.existsSync(templatesSrc)) {
-        console.error(`no emmet-gen-templates found at ${templatesSrc}`);
-        process.exit(1);
-    }
-    if (isAbsolute)
-        config.relative = false;
-    if (config.relative) {
-        config.baseUrl = path.resolve(process.cwd(), config.baseUrl);
-    }
-    else {
-        config.baseUrl = path.resolve(configLocation, '..', config.baseUrl);
-    }
-    config.templatesSource = templatesSrc;
-    return { settings: config, settingsLocation: configLocation };
+	let configLocation = `${process.cwd()}/emmet-gen-templates.json`
+	let previous = null
+	while (!fs.existsSync(configLocation)) {
+		previous = configLocation
+		configLocation = path.resolve(`${configLocation}/../../emmet-gen-templates.json`)
+		if (previous === configLocation) {
+			console.error('no emmet-gen-templates.json found')
+			process.exit(1)
+		}
+	}
+	const config = JSON.parse(fs.readFileSync(configLocation).toString())
+	const templatesSrc = path.resolve(configLocation, '..', config.templatesSource)
+	if (!fs.existsSync(templatesSrc)) {
+		console.error(`no emmet-gen-templates found at ${templatesSrc}`)
+		process.exit(1)
+	}
+	if (isAbsolute)
+		config.relative = false
+	if (config.relative) {
+		config.baseUrl = path.resolve(process.cwd(), config.baseUrl)
+	}
+	else {
+		config.baseUrl = path.resolve(configLocation, '..', config.baseUrl)
+	}
+	config.templatesSource = templatesSrc
+	return { settings: config, settingsLocation: configLocation }
 }
 //# sourceMappingURL=getConfig.js.map
