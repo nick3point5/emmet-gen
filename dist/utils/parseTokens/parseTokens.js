502fa8106bca1ab8560eeab11878ba4f07efd42a
nick3point5 <ktt4444@gmail.com>
2024-01-27 23:51:47 -0500

    build

--- dist/utils/parseTokens/parseTokens.js
+++ dist/utils/parseTokens/parseTokens.js
@@ -1,16 +1,16 @@
 import { Settings } from '../Settings/Settings.js';
 import { Template } from '../Template/Template.js';
 import { parseToken } from './parseToken/parseToken.js';
-export function parseTokens(emmetTokens, groupCount = 0) {
+export function parseTokens(emmetTokens, groupCount = 0, location = Settings.baseUrl) {
     const root = new Template({
-        name: 'root',
-        location: Settings.baseUrl,
+        name: '',
+        location: location,
         type: 'default',
         className: 'default',
     });
     let state = {
         template: root,
-        location: Settings.baseUrl,
+        location: location,
         classStack: ['default'],
         multiplyStart: 0,
         tokenIndex: 0,
@@ -23,7 +23,7 @@ export function parseTokens(emmetTokens, groupCount = 0) {
         state = instruction(state);
         state.tokenIndex++;
     }
-    if (emmetTokens[0].type === 'openGroup' && root.next) {
+    if (root.name === '' && root.next) {
         const openRoot = root.next;
         openRoot.previous = undefined;
         return openRoot;
